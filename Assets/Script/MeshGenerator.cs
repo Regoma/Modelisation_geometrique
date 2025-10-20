e5e4bb071ff75ba755bc1c15cb95fe5c1e54cae0
Regoma <94851389+Regoma@users.noreply.github.com>
2025-10-20 13:54:53 +0200

    Sphere

--- Assets/Script/MeshGenerator.cs
+++ Assets/Script/MeshGenerator.cs
@@ -10,14 +10,24 @@ using UnityEngine;
 public class MeshGenerator : MonoBehaviour
 {
     [SerializeField] private Material material;
-    [SerializeField] private float meshSize;
+    
     [Header("Plane")]
+    [SerializeField] private float meshSize;
     [SerializeField] private int nbLignes;
     [SerializeField] private int nbColones;
     [Header("Cylindre")]
     [SerializeField] private float height;
     [SerializeField] private float rayon;
-    [SerializeField] private float side;
+    [SerializeField] private int side;
+    [Header("Sphere")]
+    [SerializeField] private float sRayon;
+    [SerializeField] private int sSide;
+    [SerializeField] private float sLoop;
+    [Header("Cone")]
+    [SerializeField] private float cRayon;
+    [SerializeField] private float cHeight;
+    [SerializeField] private int cSide;
+    [SerializeField] private float cTronque;
 
     private MeshRenderer mRenderer;
     private MeshFilter mFilter;
@@ -48,11 +58,80 @@ public class MeshGenerator : MonoBehaviour
     private void Plane()
     {
         //CreatPlane();
-        CreatCylindre();
+        //CreatCylindre();
+        CreatSphere();
+        //CreatCone();
         DrawMesh();
 
     }
 
+    private void CreatCone()
+    {
+        float angle = 0;
+
+        int vUp = CreatVertex(new Vector3(0, cHeight / 2, 0));
+        int vDown = CreatVertex(new Vector3(0, -cHeight / 2, 0));
+
+        int v1 = CreatVertex(new Vector3(cRayon * Mathf.Cos(angle), -cHeight / 2, cRayon * Mathf.Sin(angle)));
+
+        for (int i = 0; i < cSide; i++)
+        {
+            
+            angle += 2 * Mathf.PI / cSide;
+            int v2 = CreatVertex(new Vector3(cRayon * Mathf.Cos(angle), -cHeight / 2, cRayon * Mathf.Sin(angle)));
+
+            CreatTriangle(v1,vUp, v2);
+            CreatTriangle(vDown, v1, v2);
+
+            v1 = v2;
+        }
+    }
+
+    private void CreatSphere()
+    {
+        float angle = 0;
+        float sHeight = sRayon * 2;
+
+        int vUp = CreatVertex(new Vector3(0, sRayon, 0));
+        int vDown = CreatVertex(new Vector3(0, -sRayon, 0));
+        float deltaTheta = Mathf.PI / sLoop;
+        float deltaPhi = 2 * Mathf.PI / sSide;
+
+        for (int l = 0; l < sLoop; l++)
+        {
+            angle = 0;
+            float theta1 = deltaTheta * l;
+            float theta2 = deltaTheta * (l + 1);
+
+            //float y1 = -sRayon + (sHeight / (sLoop)) * l;
+            //float r1 = Mathf.Sqrt(Mathf.Max(0, sRayon * sRayon - y1 * y1));
+            //float y2 = -sRayon + (sHeight / (sLoop)) * (l+1);
+            //float r2 = Mathf.Sqrt(Mathf.Max(0, sRayon * sRayon - y2 * y2));
+            float y1 = sRayon * Mathf.Cos(theta1);
+            float y2 = sRayon * Mathf.Cos(theta2);
+            float r1 = sRayon * Mathf.Sin(theta1);
+            float r2 = sRayon * Mathf.Sin(theta2);
+            Debug.Log(r1);
+
+            int v1 = CreatVertex(new Vector3(r1 * Mathf.Cos(angle), y1, r1 * Mathf.Sin(angle)));
+            int v2 = CreatVertex(new Vector3(r2 * Mathf.Cos(angle), y2, r2 * Mathf.Sin(angle)));
+
+            for (int i = 0; i < sSide; i++)
+            {
+                
+                
+                angle += 2 * Mathf.PI / sSide;
+                int v3 = CreatVertex(new Vector3(r2 * Mathf.Cos(angle),y2, r2 * Mathf.Sin(angle)));
+                int v4 = CreatVertex(new Vector3(r1 * Mathf.Cos(angle), y1, r1 * Mathf.Sin(angle)));
+                CreatTriangle(v2, v1, v4);
+                CreatTriangle(v3, v2, v4);
+
+                v1 = v4;
+                v2 = v3;
+
+            }
+        }
+    }
 
     private void CreatCylindre()
     {
