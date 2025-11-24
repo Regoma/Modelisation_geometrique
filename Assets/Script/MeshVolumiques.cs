d4e59fe82a86e99c1c1181e6e74543984982ab91
Regoma <94851389+Regoma@users.noreply.github.com>
2025-11-24 12:07:01 +0100

    intersection

--- Assets/Script/MeshVolumiques.cs
+++ Assets/Script/MeshVolumiques.cs
@@ -3,35 +3,74 @@ using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
+public enum Operation
+{
+    Union,
+    Intersection
+}
+
 public class MeshVolumiques : MonoBehaviour
 {
+    public Operation operation;
+    public float size;
+    public float cubesize;
     public GameObject voxelPrefab;
     public int resolution;
     public List<Sphere> meshs;
+    public Transform brush;
     private Node tree;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         foreach (Sphere sphere in meshs) 
             sphere.center += transform.position;
 
-        tree = new Node(transform.position,20,0);
+        tree = new Node(transform.position,size,0);
         SphereNode(tree);
         NodeDisplay(tree);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (Transform c in GetComponentInChildren<Transform>())
+            {
+                Destroy(c.gameObject);
+            }
+            foreach (Sphere sphere in meshs)
+                sphere.center += transform.position;
+
+            tree = new Node(transform.position, size, 0);
+            SphereNode(tree);
+            NodeDisplay(tree);
+        }
+
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(r,out RaycastHit hit, 50f));
+        {
+            Debug.Log(hit.point);
+            brush.transform.position = hit.point;
+        }
     }
+
 
     private void NodeDisplay(Node node)
     {
-        if(node.voxel)
+        /*
+        foreach (Transform c in GetComponentInChildren<Transform>())
+        {
+            Destroy(c.gameObject);
+        }
+        */
+        if (node.voxel)
         {
             GameObject voxel = Instantiate(voxelPrefab, node.position, new Quaternion(0, 0, 0, 0),transform);
-            voxel.transform.localScale = node.size * Vector3.one;
+            voxel.transform.localScale = node.size * Vector3.one * cubesize;
             return;
         }
 
@@ -42,11 +81,98 @@ public class MeshVolumiques : MonoBehaviour
             NodeDisplay(child);
 
     }
+    private void SphereNode(Node node)
+    {
+        float halfDiag = node.size * Mathf.Sqrt(3f) * 0.5f;
 
+        bool insideAtLeastOne = false;
+        bool outsideAll = true;
+        bool insideAll = true;
+        bool outsideAny = false;
+
+        if(operation == Operation.Union)
+        {
+            foreach (Sphere sphere in meshs)
+            {
+                float dist = Vector3.Distance(node.position, sphere.center);
+
+
+                if (dist + halfDiag < sphere.radius)
+                    insideAtLeastOne = true;
+
+
+                if (!(dist - halfDiag > sphere.radius))
+                    outsideAll = false;
+
+            }
+            if (insideAtLeastOne)
+            {
+                node.leaf = true;
+
+                node.voxel = true;
+                node.childs = null;
+                return;
+            }
+            if (outsideAll)
+            {
+                node.leaf = true;
+                node.voxel = false;
+                node.childs = null;
+                return;
+            }
+        }
+        else if (operation == Operation.Intersection)
+        {
+            foreach(Sphere sphere in meshs)
+            {
+                float dist = Vector3.Distance(node.position, sphere.center);
+
+                if (!(dist + halfDiag < sphere.radius))
+                    insideAll = false;
+
+
+                if (dist - halfDiag > sphere.radius)
+                    outsideAny=true;
+
+            }
+
+            if (outsideAny)
+            {
+                node.leaf = true;
+                node.voxel = false;
+                node.childs = null;
+                return;
+            }
+
+            if (insideAll)
+            {
+                node.leaf = true;
+                node.voxel = true;
+                node.childs = null;
+                return;
+            }
+
+        }
+
+
+
+        
+        if (node.depth >= resolution)
+            return;
+
+        NodeSubdivide(node);
+
+        foreach (Node child in node.childs)
+            SphereNode(child);
+    }
+
+    /*
     private void SphereNode(Node node)
     {
         foreach(Sphere sphere in meshs)
         {
+            if (node.voxel == true)
+                continue;
             float sphereDist = Vector3.Distance(node.position, sphere.center);
 
             //interieur
@@ -57,8 +183,15 @@ public class MeshVolumiques : MonoBehaviour
                 node.childs = null;
                 return;
             }
+            else if(sphereDist - node.size * Mathf.Sqrt(3f) * 0.5f > sphere.radius)
+            {
+                node.leaf = true;
+                node.voxel = false;
+                node.childs = null;
+                return;
+            }
         }
-
+        
         foreach (Sphere sphere in meshs)
         {
             float sphereDist = Vector3.Distance(node.position, sphere.center);
@@ -71,7 +204,7 @@ public class MeshVolumiques : MonoBehaviour
                 return;
             }
         }
-
+        
         if (node.depth>= resolution)
             return;
 
@@ -80,6 +213,7 @@ public class MeshVolumiques : MonoBehaviour
         foreach(Node child in node.childs)
             SphereNode(child);
     }
+    */
 
     private void NodeSubdivide(Node node)
     {
@@ -105,6 +239,20 @@ public class MeshVolumiques : MonoBehaviour
             return;
 
         DrawGizmosNode(tree);
+
+        foreach (Sphere sphere in meshs)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(sphere.center,sphere.radius);
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color= Color.red;
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Gizmos.DrawLine(pos, pos+ Camera.main.transform.forward *50f);
     }
 
     private void DrawGizmosNode(Node node)
@@ -126,7 +274,6 @@ public class MeshVolumiques : MonoBehaviour
     }
 }
 
-[System.Serializable]
 public class Node
 {
     public bool leaf;
@@ -135,7 +282,7 @@ public class Node
     public float size;
     public int depth;
     public Node[] childs;
-
+    public float weight;
 
 
     public Node(Vector3 pos, float s, int d)
