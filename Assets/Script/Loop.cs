1b7c023240b9869682f4cd42c90ad9beb46e411e
Regoma <quentinbosia@yahoo.fr>
2026-01-21 19:24:59 +0100

    fix loop

--- Assets/Script/Loop.cs
+++ Assets/Script/Loop.cs
@@ -8,7 +8,6 @@ using UnityEngine;
 
 public class Loop : MonoBehaviour
 {
-    [SerializeField][Range(0, 3)] private int iteration = 1;
     [SerializeField] private Material material;
     [SerializeField] private string fileName;
     private MeshRenderer mRenderer;
@@ -19,6 +18,7 @@ public class Loop : MonoBehaviour
     private List<int> triangles = new List<int>();
     List<int> oldTriangles = new List<int>();
     List<Vector3> oldVertices = new List<Vector3>();
+    Dictionary<(int, int), int> edgeCache = new Dictionary<(int, int), int>();
     void Start()
     {
         Init();
@@ -83,182 +83,110 @@ public class Loop : MonoBehaviour
     
     private void SubdivideMesh()
     {
-        for (int subdivide = 0; subdivide < iteration; subdivide++)
-        {
+            edgeCache = new Dictionary<(int, int), int>();
             oldTriangles = new List<int>(triangles);
             oldVertices = new List<Vector3>(vertices);
+            int originalVertexCount = oldVertices.Count;
             triangles.Clear();
             for (int i = 0; i < oldTriangles.Count; i += 3)
             {
-                Vector3 v1 = oldVertices[oldTriangles[i]];
-                Vector3 v2 = oldVertices[oldTriangles[i + 1]];
-                Vector3 v3 = oldVertices[oldTriangles[i + 2]];
+                int a = oldTriangles[i];
+                int b = oldTriangles[i + 1];
+                int c = oldTriangles[i + 2];
 
-                Vector3 e1 = v2 - v1;
-                Vector3 e2 = v3 - v2;
-                Vector3 e3 = v1 - v3;
+                int ab = GetEdgePoint(a, b);
+                int bc = GetEdgePoint(b, c);
+                int ca = GetEdgePoint(c, a);
 
-                int _v1 = CreatVertex(e1 * 0.5f + v1);
-                int _v2 = CreatVertex(e2 * 0.5f + v2);
-                int _v3 = CreatVertex(e3 * 0.5f + v3);
-
-                CreatTriangle(oldTriangles[i], _v1, _v3);
-                CreatTriangle(_v1, oldTriangles[i + 1], _v2);
-                CreatTriangle(_v3, _v2, oldTriangles[i + 2]);
-                CreatTriangle(_v1, _v2, _v3);
-
+                CreatTriangle(a, ab, ca);
+                CreatTriangle(b, bc, ab);
+                CreatTriangle(c, ca, bc);
+                CreatTriangle(ab, bc, ca);
             }
 
-            List<Vector3> newVertices = new List<Vector3>();
-            for (int j = 0; j < oldVertices.Count; j++)
-            {
-                List<int> neighbor = GetNeighborVertices(j);
-                Debug.Log("V_" + j + ": ");
-                Vector3 pos = oldVertices[j];
-                foreach (int i in neighbor)
-                {
-                    pos += oldVertices[i];
-                    Debug.Log(i);
-                }
-
-                pos /= neighbor.Count + 1;
-
-                vertices[j] = pos;
-            }
-        }
-        DrawMesh();
-    }
-    /*
-    private void SubdivideMesh()
-    {
-        for (int subdivide = 0; subdivide < iteration; subdivide++)
-        {
-            oldTriangles = new List<int>(triangles);
-            oldVertices = new List<Vector3>(vertices);
-
-            triangles.Clear();
-            vertices = new List<Vector3>(oldVertices); // IMPORTANT
-
-            for (int i = 0; i < oldTriangles.Count; i += 3)
-            {
-                int i1 = oldTriangles[i];
-                int i2 = oldTriangles[i + 1];
-                int i3 = oldTriangles[i + 2];
-
-                Vector3 v1 = oldVertices[i1];
-                Vector3 v2 = oldVertices[i2];
-                Vector3 v3 = oldVertices[i3];
-
-                int m12 = CreatVertex((v1 + v2) * 0.5f);
-                int m23 = CreatVertex((v2 + v3) * 0.5f);
-                int m31 = CreatVertex((v3 + v1) * 0.5f);
-
-                CreatTriangle(i1, m12, m31);
-                CreatTriangle(m12, i2, m23);
-                CreatTriangle(m31, m23, i3);
-                CreatTriangle(m12, m23, m31);
-            }
-
-            // Lissage UNIQUEMENT des anciens sommets
-            for (int j = 0; j < oldVertices.Count; j++)
+            for (int j = 0; j < originalVertexCount; j++)
             {
                 List<int> neighbors = GetNeighborVertices(j);
-                if (neighbors.Count == 0) continue;
+                int n = neighbors.Count;
 
-                Vector3 pos = oldVertices[j];
-                foreach (int n in neighbors)
-                    pos += oldVertices[n];
+                float beta = (n == 3) ? 3f / 16f : 3f / (8f * n);
 
-                pos /= (neighbors.Count + 1);
-                vertices[j] = pos;
+                Vector3 newPos = (1 - n * beta) * oldVertices[j];
+                foreach (int v in neighbors)
+                    newPos += beta * oldVertices[v];
+
+                vertices[j] = newPos;
             }
-        }
-
+        
         DrawMesh();
     }
-    */
 
     private List<int> GetNeighborVertices(int index)
     {
-        List<int> verticesN = new List<int>();
+        List<int> neighbors = new List<int>();
+
         for (int i = 0; i < oldTriangles.Count; i += 3)
         {
-            if(oldTriangles[i] == index)
+            int a = oldTriangles[i];
+            int b = oldTriangles[i + 1];
+            int c = oldTriangles[i + 2];
+
+            if (a == index)
             {
-                if(!verticesN.Contains(oldTriangles[i+1]))
-                    verticesN.Add(oldTriangles[i+1]);
-                if(!verticesN.Contains(oldTriangles[i+2]))
-                    verticesN.Add(oldTriangles[i+2]);
+                if (b < oldVertices.Count) neighbors.Add(b);
+                if (c < oldVertices.Count) neighbors.Add(c);
             }
-            if (oldTriangles[i+1] == index)
+            else if (b == index)
             {
-                if(!verticesN.Contains(oldTriangles[i+2]))
-                    verticesN.Add(oldTriangles[i+2]);
-                if(!verticesN.Contains(oldTriangles[i]))
-                    verticesN.Add(oldTriangles[i]);
+                if (a < oldVertices.Count) neighbors.Add(a);
+                if (c < oldVertices.Count) neighbors.Add(c);
             }
-            if(oldTriangles[i+2] == index)
+            else if (c == index)
             {
-                if(!verticesN.Contains(oldTriangles[i + 1]))
-                verticesN.Add(oldTriangles[i+1]);
-                if (!verticesN.Contains(oldTriangles[i]))
-                    verticesN.Add(oldTriangles[i]);
+                if (a < oldVertices.Count) neighbors.Add(a);
+                if (b < oldVertices.Count) neighbors.Add(b);
             }
         }
-        return verticesN;
+
+        return neighbors.Distinct().ToList();
     }
+    int GetEdgePoint(int a, int b)
+    {
+        if (a > b) (a, b) = (b, a);
+
+        if (edgeCache.TryGetValue((a, b), out int index))
+            return index;
+
+        Vector3 mid = (oldVertices[a] + oldVertices[b]) * 0.5f;
+        index = CreatVertex(mid);
+        edgeCache[(a, b)] = index;
+        return index;
+    }
+
 
     private int CreatVertex(Vector3 pos)
     {
         Vector3 roundPos = RoundVector(pos);
 
-        if (!vertices.Contains(roundPos))
+        if (!vertices.Contains(pos))
         {
-            vertices.Add(roundPos);
+            vertices.Add(pos);
         }
 
-        return vertices.IndexOf(roundPos);
+        return vertices.IndexOf(pos);
 
-    }
-
-    private bool ContainsPos(Vector3 pos)
-    {
-        pos *= 10000;
-        Vector3Int position = Vector3Int.FloorToInt(pos);
-        foreach(Vector3 v in vertices)
-        {
-            Vector3Int vPos = Vector3Int.FloorToInt(v * 10000);
-            if(pos == v)
-                return true;
-            /*
-            if (Vector3.Distance(pos, v) <0.01f)
-                return true;
-            */
-        }
-
-        return false;
     }
 
     private Vector3 RoundVector(Vector3 pos)
     {
-        Vector3 v = pos * 10000;
+        Vector3 v = pos * 1000;
         Vector3 roundPos = new Vector3(
         v.x = Mathf.Round(v.x),
         v.y = Mathf.Round(v.y),
         v.z = Mathf.Round(v.z));
-        roundPos /= 10000;
+        roundPos /= 1000;
         return roundPos;
 
-    }
-
-    private int IndexOfPos(Vector3 pos)
-    {
-        for (int i = 0; i < vertices.Count; i++)
-        {
-            if(Vector3.Distance(pos, vertices[i]) < 0.01f)
-                return i;
-        }
-        return 0;
     }
 
     private void CreatTriangle(int v1, int v2, int v3)
